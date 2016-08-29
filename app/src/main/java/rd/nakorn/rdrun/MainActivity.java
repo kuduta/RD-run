89a5daee5a818d3559025b12b7a124b311d7ff52
kuduta <kuduta@gmail.com>
2016-08-29 14:28:37 +0700

    Fist Intent

--- app/src/main/java/rd/nakorn/rdrun/MainActivity.java
+++ app/src/main/java/rd/nakorn/rdrun/MainActivity.java
@@ -1,7 +1,9 @@
 package rd.nakorn.rdrun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } // Main Method
+
+    // Get Event from Click button SignUp
+
+    public void ClickSignUpMain(View view){
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+    // Get Event from Click button SignIn
+    public  void ClickSignInMain(View view){
+
+    }
+
+}   // Main Class นี่คือ คลาสหลัก
