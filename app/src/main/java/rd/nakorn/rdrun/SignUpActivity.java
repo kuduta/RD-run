6fe8c62d6fc9de521b8aa9502acef025e3d68c90
kuduta <kuduta@gmail.com>
2016-08-30 10:19:33 +0700

    Bind widget

--- app/src/main/java/rd/nakorn/rdrun/SignUpActivity.java
+++ app/src/main/java/rd/nakorn/rdrun/SignUpActivity.java
@@ -2,12 +2,38 @@ package rd.nakorn.rdrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit ประการตัวแปร
+    private EditText nameEditText, surnameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avata1RadioButton, avata2RadioButton, avata3RadioButton, avata4RadioButton, avata5RadioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        // bind or initial widget คือผูกความสัมพันธ์ ระหว่างตัวแปร และ widget
+        nameEditText = (EditText) findViewById(R.id.editText);  // R is Read
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }//End Main Method
+    public void clickSignUpSign(View view) {
+
+    } // end clickSign
+} //End Main class
