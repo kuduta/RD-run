6ab461d0a5454be21c5185fc3e1f8f5a4abf7516
kuduta <kuduta@gmail.com>
2016-08-31 13:20:56 +0700

    load logo  Image form server

--- app/src/main/java/rd/nakorn/rdrun/MainActivity.java
+++ app/src/main/java/rd/nakorn/rdrun/MainActivity.java
@@ -4,14 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Expilcit
+    private ImageView imageView;
+    private EditText useEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        useEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
+        //Load Image from Server
+        Picasso.with(this).load("http://swiftcodingthai.com/rd/Image/rd_logo.png")
+                .resize(150,150).into(imageView);
     } // Main Method
+
+    public void clickSignInMain(View view) {
+
+
+
+    }
 
     // Get Event from Click button SignUp
 
